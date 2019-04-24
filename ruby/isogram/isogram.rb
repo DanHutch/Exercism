b2610bce82df2f71b2d53330ee6e8c379936cb5b
Dan Hutch <danhutch5280@gmail.com>
2019-04-24 16:51:35 -0600

    Write shorter solution

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -2,11 +2,17 @@ require 'pry'
 class Isogram
 
   def self.isogram?(string_in)
-    string_in.gsub(" ", "").gsub("-", "").downcase.chars.reduce({}) do |acc, char|
-      return false if acc[char]
-      acc[char] = true
-      acc
+    bools = [0..128]
+
+    string_in.gsub(" ", "").gsub("-", "").downcase.chars.each do |char|
+      bools[char.ord] == true ? (return false) : bools[char.ord] = true
     end
+
+    # string_in.gsub(" ", "").gsub("-", "").downcase.chars.reduce({}) do |acc, char|
+    #   return false if acc[char]
+    #   acc[char] = true
+    #   acc
+    # end
   end
 
 
